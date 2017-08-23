627123b2051baf30a4831ff53776a4b7cfdf4a79
ameydeshpande30 <ameydeshpande30@gmail.com>
2017-08-23 16:23:52 +0530

    Update admin.py

--- Clash/round1/admin.py
+++ Clash/round1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Question,Players
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Players)
