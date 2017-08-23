b77ef7192eee8d6fce4296ff995ef4deae593aa2
ameydeshpande30 <ameydeshpande30@gmail.com>
2017-08-23 18:15:07 +0530

    Update urls.py

--- Clash/Clash/urls.py
+++ Clash/Clash/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from round1 import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='0'),
