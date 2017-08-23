47529d961c0ec0d75c7088fccc9740eb1cc19bc2
ameydeshpande30 <ameydeshpande30@gmail.com>
2017-08-23 16:21:31 +0530

    Update views.py

--- Clash/round1/views.py
+++ Clash/round1/views.py
@@ -8,7 +8,7 @@ from round1.models import Players, Question
 def index(request):
     return render(request, 'signup.html', )
 def signup(request):
-    uname = request.POST.get("uname")
+    uname = request.POST['uname']
     password = request.POST.get("pass")
     p1name = request.POST.get("p1name")
     p2name = request.POST.get("p2name")
@@ -17,23 +17,42 @@ def signup(request):
     p1mno = request.POST.get("p1mno")
     p2mno = request.POST.get("p1mno")
     user = User.objects.create_user(username=uname, email=p1email, password=password)
-    question=Question(question="a", optiona="a", optionb="a",optionc="a",optiond="a", ans="c")
-    question.save()
-    user_object=Players(pid=user, p1name=p1name, p2name=p2name, p1email=p1email, p2email=p2email, p1phone=p1mno, p2phone=p2mno,score=0,que=question)
+    user_object=Players.objects.create(pid=user, p1name=p1name, p2name=p2name, p1email=p1email, p2email=p2email, p1phone=p1mno, p2phone=p2mno,score=0)
     user_object.save()
     u2 = authenticate(request, username=uname, password=password)
     login(request, u2)
-    return render(request, 'rules.html', )
-def login_page(request):
-    return render(request, 'login.html', )
+    return render(request, 'rules.html', locals())
 
+
+def login_page(request):
+    return render(request, 'login.html', locals())
+
+
+def question(request):
+    a=request.POST.get("rb")
+    if a is not None:
+        u = request.user.players
+        u.score=u.score+1
+        u.save()
+        table_list = Question.objects.all()
+        item = table_list[1]
+    else:
+        table_list = Question.objects.all()
+        item = table_list[0]
+    context = {'item': item}
+    return render(request,'question.html',context)
+
+def ans(request):
+    rb = request.POST.get("rb")
+    return HttpResponse(rb)
 def check(request):
     uname = request.POST.get("uname")
     password = request.POST.get("pass")
     u2 = authenticate(request, username=uname, password=password)
     if u2 is not None:
         login(request, u2)
-        return render(request, 'rules.html', )
+        return render(request, 'rules.html', locals())
     else:
         return HttpResponse("login Fail")
+
 
