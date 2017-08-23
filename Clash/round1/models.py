b0b23675b34adc1d5876552e778a349d804a12a0
ameydeshpande30 <ameydeshpande30@gmail.com>
2017-08-23 16:25:33 +0530

    Update models.py

--- Clash/round1/models.py
+++ Clash/round1/models.py
@@ -10,14 +10,23 @@ class Question(models.Model):
     optionc = models.CharField(max_length=100)
     optiond = models.CharField(max_length=100)
     ans = models.CharField(max_length=100)
+    qlevel = models.BooleanField(default=0)  # 0 for juniors 1 for seniors
 
 class Players(models.Model):
-    pid=models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    pid=models.OneToOneField(User)
     p1name=models.CharField(max_length=44)
     p2name = models.CharField(max_length=44)
     p1email = models.EmailField(max_length=44)
     p2email = models.EmailField(max_length=44)
     p1phone = models.CharField(max_length=44)
     p2phone = models.CharField(max_length=44)
-    score = models.IntegerField()
-    que = models.ForeignKey(Question)
+    score = models.IntegerField(default=0)
+    level = models.BooleanField(default=0)  #0 for juniors 1 for seniors
+    #harmonic = models.BooleanField(default=0)
+    #skip-remain = models.BooleanField(default=0)
+    #skipcount
+
+
+class Qattempt(models.Model):
+    question = models.ForeignKey(Question)
+    user = models.ForeignKey(User)
