c446527850e71fd4731c01de769e7946ffa04765
ameydeshpande30 <ameydeshpande30@gmail.com>
2017-08-24 00:53:59 +0530

    Update views.py

--- Clash/round1/views.py
+++ Clash/round1/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login,logout
-from round1.models import Players, Question
+from round1.models import Players, Question, Qattempt
+import random
 
 # Create your views here.
 def index(request):
@@ -26,39 +27,56 @@ def signup(request):
 
 def login_page(request):
     return render(request, 'login.html', locals())
+def randomquestiongenrate(request):
+    t = random.randint(2,5)
+    que =Question.objects.get(id=t)
+    qtemp=Qattempt.objects.filter(user=request.user)
+    for q in qtemp:
+        if q.question.id == que.id:
+             randomquestiongenrate(request)
+    return que
 
 
+def logoutfunc(request):
+    score=request.user.players.score
+    logout(request)
+    return HttpResponse(score)
 def question(request):
-    a=request.POST.get("rb")
-    if a is not None:
-        u = request.user.players
-        u.score=u.score+1
-        u.save()
-        table_list = Question.objects.all()
-        item = table_list[1]
-    else:
-        table_list = Question.objects.all()
-        item = table_list[0]
+    if 'submit' in request.POST:
+        eval(request)
+    if 'skip' in request.POST:
+        skip(request)
+    que=randomquestiongenrate(request)
+    qtemp=Qattempt(question=que,user=request.user)
+    qtemp.save()
+    item=que
     context = {'item': item}
-    return render(request,'question.html',context)
+    return render(request, 'question.html', context)
 
-def ans(request):
+def eval(request):
     rb = request.POST.get("rb")
-    return HttpResponse(rb)
-def check(request):
-    uname = request.POST.get("uname")
-    password = request.POST.get("pass")
-    u2 = authenticate(request, username=uname, password=password)
-    if u2 is not None:
-        login(request, u2)
-        return render(request, 'rules.html', locals())
-    else:
-        return HttpResponse("login Fail")
+    if rb is not None:
+        u = request.user.players
+        qtemp = Qattempt.objects.filter(user=request.user)
+        qtemp=qtemp.last()
+        ans=qtemp.question.ans
+        if rb == ans:
+            u.score = u.score + 4
+            u.skipcount = u.skipcount + 1
+        else:
+            u.score = u.score - 2
+            u.skipcount = 0
+        u.save()
+
+
 
 def skip(request):
-    u = request.user.players
-    u.score = u.score + 1
-    u.save()
+    request.user.players.skipactive = 0
+
+def skipmakeactive(request):
+    request.user.players.skipcount=request.user.players.skipcount+1
+    if request.user.players.skipcount == 3:
+        request.user.players.skipactive = 1
 
 
 def evaluate(request):
@@ -70,8 +88,26 @@ def evaluate(request):
         ans = qtemp.question.ans
         if (rb == ans):
             u.score = u.score + 4
-            u.save()
+            skipmakeactive(request)
         else:
             u.score = u.score - 2
-            u.save()
+            request.user.players.skipcount=0
+    u.save()
+
+def ans(request):
+    rb = request.POST.get("rb")
+    return  HttpResponse(rb)
+
+
+
+def check(request):
+    uname = request.POST.get("uname")
+    password = request.POST.get("pass")
+    u2 = authenticate(request, username=uname, password=password)
+    if u2 is not None:
+        login(request, u2)
+        return render(request, 'rules.html', locals())
+    else:
+        return HttpResponse("login Fail")
+
 
